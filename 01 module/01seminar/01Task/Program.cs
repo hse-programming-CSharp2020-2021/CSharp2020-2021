05cf0301e83f16620a85ff25575697b37d869742
mkgordenko@edu.hse.ru <keqpos-8suvzu-Gahsiv>
2020-09-05 00:01:57 +0300

    clean

---
+++ 01 module/01seminar/01Task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace seminar
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
